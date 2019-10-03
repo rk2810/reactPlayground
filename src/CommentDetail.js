a4a4c8350c50d56ceea7fcb0473acff7b8e3dfba
rk2810 <rohan.kumquat@gmail.com>
2019-10-03 15:44:47 +0530

    Component reusability and using props.
    Signed-off-by: rk2810 <rohan.kumquat@gmail.com>

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,21 +1,21 @@
-import  React from 'react';
-import faker from 'faker';
+import React from 'react';
 
-const CommentDetail = () => {
-return (
+const CommentDetail = (props) => {
+  console.log(props)
+  return (
     <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">{faker.name.firstName()}</a>
-          <div className="metadata">
-            <span className="date">Today at 6:00 PM</span>
-          </div>
-          <div className="comment text">{faker.lorem.sentence()}</div>
+      <a href="/" className="avatar">
+        <img alt="avatar" src={props.avatar} />
+      </a>
+      <div className="content">
+        <a href="/" className="author">{props.author}</a>
+        <div className="metadata">
+          <span className="date">{props.timeAgo}</span>
         </div>
+        <div className="comment text">{props.content}</div>
       </div>
-)
+    </div>
+  )
 }
 
 export default CommentDetail;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
+      <CommentDetail author={faker.name.firstName()} timeAgo="Today at 7:00PM" avatar={faker.image.avatar()} content={faker.lorem.sentence()} />
+      <CommentDetail author={faker.name.firstName()} timeAgo="Today at 2:00AM" avatar={faker.image.avatar()} content={faker.lorem.sentence()} />
+      <CommentDetail author={faker.name.firstName()} timeAgo="Yesterday at 5:00PM" avatar={faker.image.avatar()} content={faker.lorem.sentence()} />
     </div>
   );
 };
